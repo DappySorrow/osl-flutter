099b2eff0f4e4d834378d5c802529683206d8eeb
Jean-Simon Barbeau Barbeau <DappySorrow@gmail.com>
2024-09-28 14:19:10 -0400

    adding tooltips

--- portail_osl/lib/main.dart
+++ portail_osl/lib/main.dart
@@ -45,10 +45,12 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-  Widget buildButton(String url, String imagePath) {
-    return SizedBox(
-      width: 250, // Set the desired width
-      height: 250, // Set the desired height
+  Widget buildButton(String url, String imagePath, String tooltipMessage) {
+  return SizedBox(
+    width: 250, // Set the desired width
+    height: 250, // Set the desired height
+    child: Tooltip(
+      message: tooltipMessage, // Set the tooltip message
       child: ElevatedButton(
         onPressed: () {
           openDefaultBrowser(url);
@@ -66,8 +68,10 @@ class _MyHomePageState extends State<MyHomePage> {
           fit: BoxFit.contain, // Adjust this as needed
         ),
       ),
-    );
-  }
+    ),
+  );
+}
+
 
 
   @override
@@ -92,9 +96,15 @@ Widget build(BuildContext context) {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                buildButton("https://online.adp.com/signin/v1/?APPID=WFNPortal&productId=80e309c3-7085-bae1-e053-3505430b5495&returnURL=https://workforcenow.adp.com/&callingAppId=WFN", "assets/adp_logo.png"),
-                buildButton("msteams://", "assets/microsoft_teams_logo.png"),
-                buildButton("https://outlook.office.com/", "assets/outlook_logo.png"),
+                buildButton("https://online.adp.com/signin/v1/?APPID=WFNPortal&productId=80e309c3-7085-bae1-e053-3505430b5495&returnURL=https://workforcenow.adp.com/&callingAppId=WFN", "assets/adp_logo.png", "ADP"),
+                buildButton("msteams://", "assets/microsoft_teams_logo.png", "Microsoft Teams"),
+                ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                buildButton("https://outlook.office.com/", "assets/outlook_logo.png", "Outlook"),
+                buildButton("https://loop.cloud.microsoft/p/eyJ1IjoiaHR0cHM6Ly9vc2xycy5zaGFyZXBvaW50LmNvbS9jb250ZW50c3RvcmFnZS9DU1BfMzg1NWRhMDItN2UzMC00OTc3LTk0ZTItYjM5MjcxMjkwMTU5P25hdj1jejBsTWtaamIyNTBaVzUwYzNSdmNtRm5aU1V5UmtOVFVDVTFSak00TlRWa1lUQXlKVEpFTjJVek1DVXlSRFE1TnpjbE1rUTVOR1V5SlRKRVlqTTVNamN4TWprd01UVTVKbVE5WWlVeU1VRjBjRlpQUkVJbE1rUmtNRzFWTkhKUFUyTlRhMEpYWVZaM2RuVXljMFpxYkU5c2JsZFlkVlpyUTBOeVdqRjRSV1ptU1ZreVVWRTJkVEl3VDJneFFrVlhiQ1ptUFRBeFZFbFJUVEpOVEZkYVdrUkpWbHBYVFVsYVJVeFFWMUF5UkV4S1MxaFdOVmdtWXowbE1rWT0ifQ%3D%3D", "assets/loop.png", "Ronde de sécurité"),
               ],
             ),
           ],
